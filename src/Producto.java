086a6c8604e130630b1c8df7c34c356074c6fa88
Marcos Prosperi <mprosperi@gmail.com>
2017-09-08 14:32:28 -0300

    first commit

---
+++ src/Producto.java
@@ -0,0 +1,16 @@
+
+public class Producto {
+	private int cod;
+	private String descripcion;
+	private float precioUnit;
+	
+	public Producto(int val1, String str, Float val2) {
+		cod = val1;
+		descripcion = str;
+		precioUnit = val2;
+	}
+	
+	public float valorizar(int cant) {
+			return cant * precioUnit;
+	}
+}
