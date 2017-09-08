c1c8e70b01a306130db7e5577d12ad43517fd358
Marcos Prosperi <mprosperi@gmail.com>
2017-09-08 14:36:58 -0300

    clase consumo

--- src/Consumo.java
+++ src/Consumo.java
@@ -1,4 +1,12 @@
 
 public class Consumo {
-
+	private Producto producto;
+	private int cant;
+	private float importe;
+	
+	public consumo(Producto prod, int valor) {
+		producto = prod;
+		cant = valor;
+		importe = prod.valorizar(cant);
+	}
 }
